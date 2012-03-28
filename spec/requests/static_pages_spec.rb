0e21b466a865e828dc7b2e1351ce1778c88d2d7b
Robin Janse <robin.janse@helderberg.nl>
2012-03-28 11:52:42 +0200

    More dynamic template

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,23 +2,38 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Home'" do
       visit '/static_pages/home'
-      page.should have_content('Sample App')
+      page.should have_selector('h1', :text => 'Home')
+    end
+    
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => 'Test App | Home')
     end
   end
 
   describe "Help page" do
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_content('Help')
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "Test App | Help")
     end
   end
   
   describe "About page" do
-    it "should have the content 'About Us'" do
+    it "should have the h1 'About'" do
       visit '/static_pages/about'
-      page.should have_content('About Us')
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "Test App | About Us")
     end
   end
 end
